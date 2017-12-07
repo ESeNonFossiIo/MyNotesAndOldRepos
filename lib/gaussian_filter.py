d3728626aec3549bc15f1d594a91f137a82d790b
ESeNonFossiIo <esenonfossiio@gmail.com>
2017-12-07 19:56:10 +0100

    Add nonmax_suppression and thresholding

--- lib/gaussian_filter.py
+++ lib/gaussian_filter.py
@@ -7,9 +7,9 @@ def g(x, mu, sigma):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sigma, 2.)))
 
 def gauss_kernel(**kwargs):
-    s       = kwargs.get('size',    9)
+    s       = kwargs.get('size',    3)
     mu      = kwargs.get('mu',      0.0)
-    sigma   = kwargs.get('sigma',   3.0)
+    sigma   = kwargs.get('sigma',   2.0)
 
     x       = np.array(range(-s,s+1))
     xx      = np.array([x*x])
@@ -19,8 +19,8 @@ def gauss_kernel(**kwargs):
     return g(m, mu, sigma)
 
 def gaussian(img, **kwargs):
-    s       = kwargs.get('size',    9)
+    s       = kwargs.get('size',    3)
     mu      = kwargs.get('mu',      0.0)
-    sigma   = kwargs.get('sigma',   3.0)
+    sigma   = kwargs.get('sigma',   2.0)
 
     return convolve2d(img, gauss_kernel(size=s, mu=mu, sigma=sigma), mode="same", boundary="symm")
